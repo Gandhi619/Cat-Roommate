a8f1a05213547b4e2414a4b8a49719c9544e4bb5
Gandhi Mendoza <gm2539@nyu.edu>
2019-03-07 12:33:17 -0500

    Added Sprite Changes
    Right now all Good Answers are on right and all Bad Answers are on left. No ending yet.

---
+++ Cat Roommate Clone/Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+    
+    public Sprite[] spriteList;
+    SpriteRenderer Catsr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Catsr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GoodAnswersSpriteChange();
+        BadAnswersSpriteChange();
+
+    }
+    void GoodAnswersSpriteChange()
+    {
+        if (TextChanger.GoodAnswers == 1)
+        {
+            Catsr.sprite = spriteList[3];
+        }
+        if (TextChanger.GoodAnswers == 2)
+        {
+            Catsr.sprite = spriteList[0];
+        }
+        if (TextChanger.GoodAnswers == 3)
+        {
+            Catsr.sprite = spriteList[2];
+        }
+        if (TextChanger.GoodAnswers == 4)
+        {
+            Catsr.sprite = spriteList[5];
+        }
+        if(TextChanger.GoodAnswers == 5)
+        {
+            Catsr.sprite = spriteList[2];
+        }
+        if(TextChanger.GoodAnswers == 6)
+        {
+            Catsr.sprite = spriteList[4];
+        }
+    }
+    void BadAnswersSpriteChange()
+    {
+        if(TextChanger.BadAnswers == 1)
+        {
+            Catsr.sprite = spriteList[11];
+        }
+        if(TextChanger.BadAnswers == 2)
+        {
+            Catsr.sprite = spriteList[2];
+        }
+        if (TextChanger.BadAnswers == 3)
+        {
+            Catsr.sprite = spriteList[8];
+        }
+        if (TextChanger.BadAnswers == 4)
+        {
+            Catsr.sprite = spriteList[9];
+        }
+        if (TextChanger.BadAnswers == 5)
+        {
+            Catsr.sprite = spriteList[11];
+        }
+        if (TextChanger.BadAnswers == 6)
+        {
+            Catsr.sprite = spriteList[10];
+        }
+    }
+}
---
+++ Cat Roommate Clone/Assets/Scripts/TextChanger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextChanger: MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public string[] textStuff;
+    int linearNum = 0;
+    bool wasRightClicked;
+    bool wasLeftClicked;
+    public static int GoodAnswers = 0;
+    public static int BadAnswers = 0;
+
+    public void RightClicked()
+    {
+        wasRightClicked = true;
+        wasLeftClicked = false;
+        
+        if(wasRightClicked == true)
+        {
+            
+            GoodAnswers++;
+            Debug.Log("GoodAnswer is" + GoodAnswers);
+        }
+    }
+    public void LeftClicked()
+    {
+        wasLeftClicked = true;
+        wasRightClicked = false;
+        
+        if(wasLeftClicked == true)
+        {
+            BadAnswers++;
+            Debug.Log("BadAnswer is" + BadAnswers);
+        }
+    }
+
+
+    public void ChangeText()
+    {
+       
+        
+        linearNum++;
+
+        Debug.Log("Number is " + linearNum);
+        text.text = textStuff[(linearNum)];
+
+    }
+   
+}
